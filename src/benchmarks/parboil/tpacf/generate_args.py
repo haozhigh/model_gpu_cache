07f16254dc5e5d72c0be97e58a4c84655d92a61d
Ya Zhang <haozhigg@gmail.com>
2016-01-26 16:03:20 +0800

    Deleted bench parboil/tpacf because of its unregular operation intensity

--- src/benchmarks/parboil/tpacf/generate_args.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-print("-i ../../../../datasets/parboil/tpacf/small/input/Datapnts.1,", end = "")
-for i in range(100):
-    print("../../../../datasets/parboil/tpacf/small/input/" + "Randompnts." + str(i + 1) + ",", end = "")
-print(" -o ../../../../output/bench_output/parboil/tpacf_small.out")

5ffe32ed58d82e4e5d7022a879095e53770e3904
Ya Zhang <haozhigg@gmail.com>
2016-04-25 15:01:58 +0800

    Sync commit

--- script/draw_duration.py
+++ script/draw_duration.py
@@ -17,11 +17,13 @@ def main():
     draw_reuse_distance_duration_comparison_v2(duration_frame, "Reuse Distance Duration Comparison", path.join(dir_script, "../output/reuse_distance_duration_comparison.png"))
 
     ##  Add trace generation time to total model time
-    duration_frame['base_model'] = duration_frame['base_model'] + duration_frame['base_trace']
-    duration_frame['model'] = duration_frame['model'] + duration_frame['trace']
+    #duration_frame['base_model'] = duration_frame['base_model'] + duration_frame['base_trace']
+    #duration_frame['model'] = duration_frame['model'] + duration_frame['trace']
+
+    duration_frame['model'] = duration_frame['opt_break_trace_off']
 
     ##  Call function to draw duration comparison
-    draw_duration(duration_frame, 'Time Consumption Comparison', path.join(dir_script, "../output/duration.png"))
+    draw_duration_v2(duration_frame, 'Time Consumption Comparison', path.join(dir_script, "../output/duration.png"))
 
 if __name__ == '__main__':
     main()
